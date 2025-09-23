fe0c8b6435cd89d08d0d16043f29fdbfddc18dff
Tamal Das <tamaldas177@gmail.com>
2025-09-23 17:22:45 +0530

    created a new worker image

---
+++ deployments/initial_load_deploy.py
@@ -0,0 +1,12 @@
+from prefect import flow
+
+if __name__ == "__main__":
+    flow.from_source(
+        source="https://github.com/tdas-kolkata/market-mitra.git",
+        entrypoint="jobs/intial_load.py:load_company_details_flow",
+    ).deploy(
+        description="Loads initial comnay details",
+        work_pool_name="etl-workers",
+        name="initial-load",
+        tags=["initial", "python"]
+    )
