6e1a2d55e6150135d8d61a7a4cb39d43b44b0522
Tamal Das <tamaldas177@gmail.com>
2026-01-07 02:23:49 +0530

    handle upper and lower circuit

--- jobs/pricing/daily/load_daily_pricing.py
+++ jobs/pricing/daily/load_daily_pricing.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import dotenv
 import sys
+from sqlalchemy.dialects.postgresql import insert
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '..', '..', '..', '.env')
 dotenv.load_dotenv(dotenv_path)
@@ -42,25 +43,44 @@ def load_pricing(price_history:pd.DataFrame):
     for record in df_records:
         price_row = PriceModel(**record)
         validated_rows.append(price_row)
+        price_history = price_history.rename(
+            columns={
+                "Date": "date",
+                "Open": "open",
+                "High": "high",
+                "Low": "low",
+                "Close": "close",
+                "Volume": "volume",
+                "Dividends": "dividends",
+                "Stock Splits": "stock_splits",
+                "Stock": "stock",
+                "ISIN": "isin_code",
+            }
+        )
 
+
+    records = price_history.to_dict(orient="records")
+    engine = create_engine(os.getenv("DB_URL"))
     try:
-        engine = create_engine(os.getenv("DB_URL"))
-        # Try connecting to the database to check connectivity
-        with engine.connect() as _:
-            pass
-        SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    except Exception as conn_err:
-        logger.error(f"Database connection failed: {conn_err} 🤒")
+        with engine.begin() as conn:
+            stmt = insert(Price).values(records)
+            stmt = stmt.on_conflict_do_update(
+                index_elements=["stock", "date"],
+                set_={
+                    "open": stmt.excluded.open,
+                    "high": stmt.excluded.high,
+                    "low": stmt.excluded.low,
+                    "close": stmt.excluded.close,
+                    "volume": stmt.excluded.volume,
+                },
+            )
+            conn.execute(stmt)
+
+        logger.info(f"Upserted {len(records)} rows")
+
+    except Exception as e:
+        logger.error(f"Pricing upsert failed: {e}")
         raise
-    with SessionLocal() as session:
-        try:
-            session.bulk_insert_mappings(Price, validated_rows)
-            session.commit()
-            logger.info(f"Successfully inserted {len(validated_rows)} records 👍")
-        except Exception as e:
-            session.rollback()
-            logger.error(f"An error occurred: {e}")
-            raise e
 
 @flow(log_prints=True, flow_run_name="daily-price-{symbol}-{period}")
 def load_daily_pricing(symbol:str, isin_code:str, period: str):
@@ -70,5 +90,5 @@ def load_daily_pricing(symbol:str, isin_code:str, period: str):
     logger.info(transformed_pricing_history.info())
     load_pricing(transformed_pricing_history)
 
-# if __name__ == "__main__":
-#     load_daily_pricing(symbol="TCS.NS", isin_code="INE467B01029", period='7d')
+if __name__ == "__main__":
+    load_daily_pricing(symbol="RELINFRA.NS", isin_code="INE036A01016", period='1d')
