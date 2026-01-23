f1ceb8757795a1ad2e21cdccfac829c74d0028a6
Tamal Das <tamaldas177@gmail.com>
2026-01-24 00:23:03 +0530

    updating traefik config

--- jobs/cleanup/prefect_db_cleanup.py
+++ jobs/cleanup/prefect_db_cleanup.py
@@ -1,12 +1,14 @@
-from prefect import flow, task
+from prefect import flow, task, get_run_logger
 from sqlalchemy import create_engine
 import os
 
 @task
 def clean_up_prefect_db():
-    engine = create_engine(os.getenv("PREFECT_API_DATABASE_CONNECTION_URL"))
+    logger = get_run_logger()
+    engine = create_engine(os.getenv("PREFECT_API_DATABASE_CONNECTION_URL"), echo = True)
 
     with engine.connect() as conn:
+        logger.info("Successfully connected to prefect db")
         conn.execute("""
                     DELETE FROM task_run WHERE start_time < NOW() - INTERVAL '3 days';
                     DELETE FROM task_run_state WHERE timestamp < NOW() - INTERVAL '3 days';
@@ -23,6 +25,9 @@ def clean_up_prefect_db():
 @flow
 def clean_up_db():
     clean_up_prefect_db()
+
+if __name__ == '__main__':
+    clean_up_db()
 
 
 
