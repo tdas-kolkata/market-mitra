6b09967b1f23d064991dea6e7acaf126149513dd
Tamal Das <tamaldas177@gmail.com>
2026-02-17 16:50:10 +0530

    refactor changes

--- jobs/update_view.py
+++ jobs/update_view.py
@@ -4,7 +4,7 @@ import dotenv
 import os
 from enum import Enum
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '..', '..', '.env')
+dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
 dotenv.load_dotenv(dotenv_path)
 
 
@@ -14,12 +14,12 @@ class DB_VIEW(str, Enum):
 @task
 def update_view_task(view_name: DB_VIEW):
     logger = get_run_logger()
-    statement = text(f"refresh materialized view {view_name};")
+    statement = text(f"refresh materialized view {view_name.value};")
     engine = create_engine(os.getenv("DB_URL"))
     try:
         with engine.begin() as conn:
             conn.execute(statement)
-        logger.info(f"Successfully refreshed the {view_name}")
+        logger.info(f"Successfully refreshed the {view_name.value}")
     except Exception as e:
         logger.error(e)
 
@@ -27,4 +27,8 @@ def update_view_task(view_name: DB_VIEW):
 @flow(log_prints=True)
 def update_view_flow(view_name: DB_VIEW):
     update_view_task(view_name)
+
+
+if __name__ == '__main__':
+    update_view_flow(view_name = DB_VIEW.RISK_RETURN_VIEW)
 
